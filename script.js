7c0fabd62ec1aaf7bbef4cf6da45caa1cc5ece0c
College-guide-hub <lakshmi.01tech@gmail.com>
2025-12-28 12:35:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let colleges = [];
+
+fetch("Colleges.json")
+  .then(res => res.json())
+  .then(data => colleges = data);
+
+function searchCollege() {
+  let query = document.getElementById("search").value.toLowerCase();
+  let output = "";
+
+  colleges.forEach(college => {
+    if (
+      college.city.toLowerCase().includes(query) ||
+      college.courses.join(" ").toLowerCase().includes(query)
+    ) {
+      output += `
+        <h3>${college.name}</h3>
+        <p>City: ${college.city}</p>
+        <p>Courses: ${college.courses.join(", ")}</p>
+        <p>Fees: ${college.fees}</p>
+        <hr>
+      `;
+    }
+  });
+
+  document.getElementById("result").innerHTML =
+    output || "No college found";
+          }
